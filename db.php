8602ea0676ecf254a5677cff27754c64482652b5
Rune Heidtmann <37456552+runeheidtmann@users.noreply.github.com>
2019-03-09 13:34:02 +0100

    Update db.php

--- db.php
+++ db.php
@@ -7,14 +7,11 @@ class db{
 	private $dbname;
 
 	public function connect(){
-		$this->servername = "mysql42.unoeuro.com";
-		$this->username = "wemakebots_dk";
-		$this->password = "colafrik1";
-        $this->dbname = "wemakebots_dk_db";
-        // $this->servername = "localhost";
-		// $this->username = "root";
-		// $this->password = "";
-		// $this->dbname = "test";
+	
+         $this->servername = "localhost";
+	 $this->username = "root";
+	 $this->password = "";
+	 $this->dbname = "test";
 
 		$conn = new mysqli($this->servername,$this->username,$this->password,$this->dbname);
 		return $conn;
